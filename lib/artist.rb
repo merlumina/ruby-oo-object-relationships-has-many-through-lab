0acf4a8570d962284dc69f336bb47af6de4f1a9c
Liz <lizriderwilson@gmail.com>
2021-01-10 12:40:30 -0500

    commit tree a40d9bd2317a19371573333849d86af91176d345

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,27 @@
+class Artist
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select {|s| s.artist == self}
+    end
+
+    def genres
+        songs.collect {|s| s.genre}
+    end
+
+end
